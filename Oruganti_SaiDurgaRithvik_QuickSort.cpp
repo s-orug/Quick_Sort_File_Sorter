57fad1065664bb630ab68ecebfd4b5b9c60a8cb1
Oruganti <orugants@cocsce-l3d22-08.ds.sc.edu>
2022-11-08 16:16:22 -0500

    quicksort implementation

--- Oruganti_SaiDurgaRithvik_QuickSort.cpp
+++ Oruganti_SaiDurgaRithvik_QuickSort.cpp
@@ -2,8 +2,49 @@
 #include <cmath>
 #include <random>
 
+
+
+int partition(int array[], int low, int high){
+    int pivot = array[high], val = low - 1;
+    for (int i = low; i < high; ++i){
+        if (array[i] <= pivot){
+            ++val;
+
+            int tmp = array[i];
+            array[i] = array[val];
+            array[val] = tmp;
+        }
+    }
+
+    int tmp = array[high];
+    array[high] = array[val+1];
+    array[val+1] = tmp;
+
+    return ++val;
+
+}
+
+void sort(int array[], int low, int high){
+
+    if(low < high){
+        sort(array, low, partition(array, low, high) - 1);
+        sort(array, partition(array, low, high) - 1, high);
+    }
+}
+
 int main(int argc, char** argv)
 {
+    int array[] = {3,1,5,12,37,33,11};
+    int size = sizeof(array)/sizeof(int);
     
+    for (auto i: array)
+        std::cout << i << " ";
+    std::cout << std::endl;
+
+    sort(array, 0, size-1);
+
+    for (auto i: array)
+        std::cout << i << " ";
+    std::cout << std::endl;
     return 0;
 }
